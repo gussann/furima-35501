33fc69be367cdc238052db19c5814ec0b642b4ea
gussann <84413122+gussann@users.noreply.github.com>
2021-07-29 22:09:23 +0900

    正規表現の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,10 @@ class User < ApplicationRecord
 
   validates :nickname, :family_name, :family_name_kana, :first_name_kana, :birth_day, :first_name, presence: true
   validates :email, uniqueness: true
-  validates :email, format: { with: /\A[\w+-.]+@[a-z\d-]+(.[a-z\d-]+)*.[a-z]+\z/i }
+  validates_format_of :email, with: /\A[\w+-.]+@[a-z\d-]+(.[a-z\d-]+)*.[a-z]+\z/i
   validates :encrypted_password, length: { minimum: 6 }
-  validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates_format_of :encrypted_password, with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates :encrypted_password, confirmation: true
-  validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z }
-  validates :family_name_kana, :first_name_kana, format: { with: /\p{katakana}/ }
-  
+  validates_format_of :family_name, :first_name, with: /\A[ぁ-んァ-ン一-龥]/
+  validates_format_of :family_name_kana, :first_name_kana, with: /\p{katakana}/
 end
